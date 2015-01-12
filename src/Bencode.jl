b1017d25538eb5752a1142bc65f4763034e875cf
Joshua Miller <josh@joshmiller.io>
2015-01-11 18:26:28 -0800

    Bencode module.

---
+++ src/Bencode.jl
@@ -0,0 +1,91 @@
+module Bencode
+
+export bencode, bdecode
+
+function bencode(val::String)
+    string(length(val)) * ":" * val
+end
+
+function bencode(val::Int)
+    "i" * string(val) * "e"
+end
+
+function bencode(val::Array)
+    "l" * join(map(bencode, val)) * "e"
+end
+
+function bencode(val::Dict)
+    ks = sort(collect(keys(val)), lt = (x, y) -> isless(string(x), string(y)))
+    "d" * join(map((k) -> bencode(string(k)) * bencode(val[k]), ks)) * "e"
+end
+
+function bparseint(val::String)
+    parseint(split(val, "e")[1])
+end
+
+function bparsestring(val::String)
+    splitstr = split(val, ":", 2)
+    thislength = parseint(splitstr[1])
+    if length(splitstr[2]) > thislength
+        thisstring = splitstr[2][1:thislength]
+        return thisstring, splitstr[2][thislength + 1:end]
+    else
+        return splitstr[2]
+    end
+end
+
+function bparseint(val::String)
+    splitstr = split(val, "e", 2)
+    thisint = parseint(splitstr[1])
+    if length(splitstr[2]) > 0
+        return thisint, splitstr[2]
+    else
+        return thisint
+    end
+end
+
+function bparsearray(val::String)
+    array = Union(String,Int,Array,Dict)[]
+    while val[1] != 'e'
+        entry, val = bdecode(val)
+        push!(array, entry)
+    end
+    if length(val) > 1
+        array, val[2:end]
+    else
+        array
+    end
+end
+
+function bparsedictentry(val::String)
+    k, rest = bparsestring(val)
+    v, rest = bdecode(rest)
+    k, v, rest
+end
+
+function bparsedict(val::String)
+    dict = Dict()
+    while val[1] != 'e'
+        k, v, val = bparsedictentry(val)
+        dict[k] = v
+    end
+    if length(val) > 1
+        dict, val[2:end]
+    else
+        dict
+    end
+end
+
+function bdecode(val::String)
+    if val[1] == 'i'
+        bparseint(val[2:end])
+    elseif val[1] == 'l'
+        bparsearray(val[2:end])
+    elseif val[1] == 'd'
+        bparsedict(val[2:end])
+    else
+        bparsestring(val)
+    end
+end
+
+end
